57b34eac5ed78c87070d55a090de8ada01e9f961
Accany1 <129660499+Accany1@users.noreply.github.com>
2024-09-09 14:26:38 +0800

    Added an operate func to interprete the operation to do

--- javascript.js
+++ javascript.js
@@ -1,17 +1,35 @@
 add = (n1, n2) => {
-    return n1 + n2
+    return Number(n1) + Number(n2)
 }
 
 subtract = (n1, n2) => {
-    return n1 - n2
+    return Number(n1) - Number(n2)
 }
 
 multiply = (n1, n2) => {
-    return n1 * n2
+    return Number(n1) * Number(n2)
 }
 
 divide = (n1, n2) => {
-    return n1/n2
+    return Number(n1)/Number(n2)
 }
 
-console.log(divide(2,4))
+removeSpacesToArray = (inputString) => {
+    let newString = inputString.split('').filter(num => num !== ' ')
+    return newString
+}
+
+operate = (inputString) => {
+    let mathArray = removeSpacesToArray(inputString)
+    if (mathArray[1] === "+") {
+        return add(mathArray[0],mathArray[2])
+    } else if (mathArray[1] === "-") {
+        return subtract(mathArray[0],mathArray[2])
+    } else if (mathArray[1] === "*") {
+        return multiply(mathArray[0],mathArray[2])
+    } else if (mathArray[1] === "/") {
+        return divide(mathArray[0],mathArray[2])
+    }
+}
+
+console.log(operate("1/ 2"))
