3ab364b52116d0f749b632cb1577e2ef9bbb60a5
Accany1 <129660499+Accany1@users.noreply.github.com>
2024-09-09 14:07:20 +0800

    Added addition, substraction, multiplication and divison functions

---
+++ javascript.js
@@ -0,0 +1,17 @@
+add = (n1, n2) => {
+    return n1 + n2
+}
+
+subtract = (n1, n2) => {
+    return n1 - n2
+}
+
+multiply = (n1, n2) => {
+    return n1 * n2
+}
+
+divide = (n1, n2) => {
+    return n1/n2
+}
+
+console.log(divide(2,4))
